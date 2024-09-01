a71b4c881b0b070bd6838815d761b3d6e8496c4c
Damir Abdurakhimov <109365159+Damir94@users.noreply.github.com>
2024-09-01 17:04:07 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,11 +4,10 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.63.0"
+      source  = "hashicorp/aws"
+      version = ">= 3.0"  # Ensure you use version 3.0 or newer
     }
   }
 }
